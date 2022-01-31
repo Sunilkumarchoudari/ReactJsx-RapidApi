c149f219e95b10b989063d2b2d07bf770913383f
sunilkumarchoudari <notifysunilkumar@gmail.com>
2022-01-31 12:25:22 +0530

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 const App = () => {
     return (
-        <div>
-
-            <h1>
-                Hello world!
-            </h1>
-
-        </div>
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100' }}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
